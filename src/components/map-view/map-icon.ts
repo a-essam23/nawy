5c56e6c4548e947248523c732362b35b21afa8fc
Ahmed <aessamby@gmail.com>
2025-05-20 13:15:54 +0300

    chore: cleanup

--- src/components/map-view/map-icon.ts
+++
@@ -1,9 +0,0 @@
-export const placeMarkerIcon = () => {
-  const { Icon } = require("leaflet");
-
-  return new Icon({
-    iconUrl: "/gps.png", // Path relative to the public directory
-    iconSize: [42, 42],
-    iconAnchor: [21, 42],
-  });
-};
--- src/utils/cn.ts
+++
@@ -1,7 +0,0 @@
-import { ClassValue, clsx } from "clsx";
-import { twMerge } from "tailwind-merge";
-const cn = (...props: ClassValue[]) => {
-  return twMerge(clsx(props));
-};
-
-export default cn;
